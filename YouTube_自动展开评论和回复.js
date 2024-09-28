7c1d24437e9581fc66b4c11680f94f1d2a850615
SuperNG6 <sleele@gmail.com>
2024-09-28 20:01:04 +0800

    Update YouTube_自动展开评论和回复.js

--- YouTube_自动展开评论和回复.js
+++ YouTube_自动展开评论和回复.js
@@ -1,12 +1,28 @@
 // ==UserScript==
-// @name         YouTube 自动展开评论和回复
+// @name         YouTube Auto Expand Comments and Replies
+// @name:zh-CN   YouTube 自动展开评论和回复
+// @name:zh-TW   YouTube 自動展開評論和回覆
+// @name:ja      YouTube コメントと返信を自動展開
+// @name:ko      YouTube 댓글 및 답글 자동 확장
+// @name:es      Expansión automática de comentarios y respuestas de YouTube
+// @name:fr      Expansion automatique des commentaires et réponses YouTube
+// @name:de      Automatische Erweiterung von YouTube-Kommentaren und Antworten
 // @namespace    https://github.com/SuperNG6/YouTube-Comment-Script
 // @author       SuperNG6
-// @version      1.1
-// @description  自动展开 YouTube 视频页面的所有评论和回复
+// @version      1.2
+// @description  Automatically expand all comments and replies on YouTube video pages
+// @license MIT
+// @description:zh-CN  自动展开 YouTube 视频页面的所有评论和回复
+// @description:zh-TW  自動展開 YouTube 視頻頁面的所有評論和回覆
+// @description:ja     YouTube動画ページのすべてのコメントと返信を自動的に展開します
+// @description:ko     YouTube 동영상 페이지의 모든 댓글과 답글을 자동으로 확장합니다
+// @description:es     Expande automáticamente todos los comentarios y respuestas en las páginas de videos de YouTube
+// @description:fr     Développe automatiquement tous les commentaires et réponses sur les pages vidéo YouTube
+// @description:de     Erweitert automatisch alle Kommentare und Antworten auf YouTube-Videoseiten
 // @match        https://www.youtube.com/watch?v=*
 // @grant        none
 // ==/UserScript==
+
 
 (function() {
     'use strict';
